d6d1a4f7a97cabb7d2c3103613eec70bc143b0b2
ergunirem <iremthedeveloper@gmail.com>
2020-06-01 16:36:29 +0200

    added functions to move images

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function computerPlay() {
-    let items = ['rock', 'paper', 'scissors'];
-    return items[Math.floor(Math.random() * items.length)]
-};
-
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == 'rock' && computerSelection == 'paper') {
@@ -32,17 +27,47 @@ function playRound(playerSelection, computerSelection) {
     else {
         return "It's a tie!";
     }
+};
 
+function computerPlay() {
+    const items = ['rock', 'paper', 'scissors'];
+    return items[Math.floor(Math.random() * items.length)];
+};
+
+function moveRight(e) {
+    e.style.left = parseInt(e.style.left) + 150 + 'px';
 }
 
-const images = document.querySelectorAll('.img-left');
+function moveRightBack(e) {
+    e.style.left = parseInt(e.style.left) + -150 + 'px';
+}
+
+function moveLeft(e) {
+    e.style.right = parseInt(e.style.right) + 150 + 'px';
+}
+
+function moveLeftBack(e) {
+    e.style.right = parseInt(e.style.right) + -150 + 'px';
+}
+
 let playerScore = 0;
 let computerScore = 0;
 
+const rightImages = document.querySelectorAll('.img-right');
+rightImages.forEach(image => image.style.position= 'relative');
+rightImages.forEach(image => image.style.right= '0px');
+ 
+
+const images = document.querySelectorAll('.img-left');
+images.forEach(image => image.style.position= 'relative');
+images.forEach(image => image.style.left= '0px');
 images.forEach(image => image.addEventListener("click", function (e) {
+    //Call the play function with clicked choice and computer's random selection
     const playerSelection = e.target.id;
     const computerSelection = computerPlay();
-    const result = playRound(playerSelection, computerSelection) 
+    const result = playRound(playerSelection, computerSelection)
+    
+    //Display this round's result and updated score
     if (result.includes('win')) {
         playerScore++;
         document.getElementById("playerScore").innerHTML=playerScore;
@@ -52,7 +77,16 @@ images.forEach(image => image.addEventListener("click", function (e) {
         document.getElementById("computerScore").innerHTML=computerScore;
     }
     document.getElementById("roundResult").textContent = result;
-}))
+
+    //Move the image of player's choice forward and back
+    moveRight(image);
+    setTimeout(function () {moveRightBack(image)}, 1000);
+    
+    //Move the image of computer's choice forward and back
+    const computerSelectionImage = document.getElementById(`right ` + computerSelection)
+    moveLeft(computerSelectionImage);
+    setTimeout(function () {moveLeftBack(computerSelectionImage)}, 1000);
+}));
 
 
 
